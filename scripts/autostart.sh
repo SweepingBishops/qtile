f54c07d0d5adfd31f835487a16b75e729fce4d1e
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-02-15 18:56:10 +0530

    Commit

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -10,4 +10,3 @@ xinput set-prop 'Elan Touchpad' 'libinput Natural Scrolling Enabled' 1 &
 setxkbmap in,gr -variant eng,simple -option grp:shifts_toggle &
 numlockx on &
 /home/roshan/.myscripts/automatic_royalroad_update_checker &
-qtile cmd-obj -o group 1 -f toscreen &
