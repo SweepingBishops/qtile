6040623fa04b766217365dac374670522318f8ed
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-02-16 17:11:01 +0530

    Starts Royal Road Update checker on start

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -9,7 +9,7 @@ xinput set-prop 'Elan Touchpad' 'libinput Tapping Enabled' 1 &
 xinput set-prop 'Elan Touchpad' 'libinput Natural Scrolling Enabled' 1 &
 setxkbmap in,gr -variant eng,simple -option grp:shifts_toggle &
 numlockx on &
-#/home/roshan/.myscripts/automatic_royalroad_update_checker &
+/home/roshan/.myscripts/automatic_royalroad_update_checker &
 xidlehook \
     --not-when-fullscreen \
     --not-when-audio \
